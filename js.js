6762e0962e0b032e3dd9448425d1f175ccce2054
Krsmanovic Miroslav - GrECo Shared Services Center <m.krsmanovic@greco.services>
2020-09-01 16:41:01 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+ $("button").click(function() {
+  alert("hello");
+ });
+});
+//typed.js
+let typed = new Typed(".type", {
+ strings: ["very predictive", "new now with us"],
+ typeSpeed: 40,
+ backSpeed: 20,
+ autoInsertCss: true,
+ loop: true
+});
+
+//anime.js
+anime({
+ targets: "div.graphic",
+ translateY: [
+  { value: 20, duration: 13200 },
+  { value: 0, duration: 11200 }
+ ],
+ translateX: [
+  { value: -20, duration: 13200 },
+  { value: 0, duration: 13200 }
+ ],
+ loop: true
+});
+
+anime({
+ targets: "button",
+ opacity: [{ value: 0 }, { value: 1, duration: 6000 }],
+ translateY: [{ value: 10 }, { value: 0, duration: 6000 }]
+});
